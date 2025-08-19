4e5cf661d27ce512181c3cfb19b47d516f56e44e
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2025-08-19 15:29:12 +0000

    Old files cleanup

--- loader.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-from config import TOKEN
-
-# Инициализация бота и Dispatcher
-storage = MemoryStorage()
-bot = Bot(token=TOKEN)
-dp = Dispatcher(storage=storage)
